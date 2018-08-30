c28ce2d069d8a802b0919cb5331c3727d6fcb357
Sacha Epskamp <sacha.epskamp@gmail.com>
2018-08-30 10:33:05 +0200

    Contemporaneous multi-level models are now returned in the output

--- R/lmer_murmur.R
+++ R/lmer_murmur.R
@@ -597,7 +597,7 @@ lmer_mlVAR <-
     # Combine results:
     Results <- list(
       results = Results,
-      output = lmerResults,
+      output = list(temporal = lmerResults, contemporaneous = lmerResults2),
       fit = fit,
       data = augData,
       model = model
